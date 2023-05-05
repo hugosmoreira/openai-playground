bf94f84fd5c9b5365632813bc615ba5df91fb5bf
Hugo Moreira <hugo.n4@gmail.com>
2023-05-04 21:10:56 -0700

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
+const dotenv = require('dotenv')
 const PORT = 8000
 const express = require('express')
 const cors = require('cors')
@@ -10,4 +11,32 @@ app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
+})
+
+const OPEN_API_KEY = process.env.OPEN_API_KEY
+
+app.post('completions', async (req, res) => {
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${OPEN_API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model : 'gpt-4',
+            messages: [{role: "user", content: "How are you?"}],
+            max_tokens: 150,
+
+        })
+    }
+
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
 })
